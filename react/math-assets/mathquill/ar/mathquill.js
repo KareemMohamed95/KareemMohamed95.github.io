0680bc1f227fdd71514db1a4035bf64fef55d48c
Kareem Mohamed <kareem_basha61@yahoo.com>
2021-09-25 15:54:49 +0200

    Add MathOptions for Arabic MathQuill

--- react/math-assets/mathquill/ar/mathquill.js
+++ react/math-assets/mathquill/ar/mathquill.js
@@ -267,12 +267,12 @@ function addPhiSymbol(){
       j++;
     }
     if(mappedWord.length == 0){
-      let finalMappedWord = mqArabicMapping[latex[i]];
+      let finalMappedWord = window.mqArabicMapping[latex[i]];
       if(!(finalMappedWord === undefined || finalMappedWord === null))newLatex += finalMappedWord;
       else newLatex += latex[i];
     }
     else {
-      let finalMappedWord = mqArabicMapping[mappedWord];
+      let finalMappedWord = window.mqArabicMapping[mappedWord];
       if(!(finalMappedWord === undefined || finalMappedWord === null))newLatex += finalMappedWord;
       i = j-1;
     }
@@ -323,38 +323,6 @@ function isArabicChar(char) {
    ||char == 'ق' || char == 'ح' || char == 'ن')return true;
    return false;
 }
-var mqArabicMapping = {
-  "٠": "0",
-  "١": "1",
-  "٢": "2",
-  "٣": "3",
-  "٤": "4",
-  "٥": "5",
-  "٦": "6",
-  "٧": "7",
-  "٨": "8",
-  "٩": "9",
-  "س": "x",
-  "ص": "y",
-  "ع": "z",
-  "ر": "r",
-  "أ": "a",
-  "ب": "b",
-  "ج": "c",
-  "ث": "c",
-  "ن": "n",
-  "ح": "R",
-  "ت": "i",
-  ",": ".",
-  "،": ",",
-  "جا": "\\sin",
-  "جتا": "\\cos",
-  "ظا": "\\tan",
-  "قتا": "\\csc",
-  "قا": "\\sec",
-  "ظتا": "\\cot",
-  "ط": "\\pi"
-};
 var mqEnglishMapping = {
   "0": "٠",
   "1": "١",
